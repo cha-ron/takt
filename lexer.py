f694aa22e7ce475a36e771804fc1992b6559249a
McTavish McArdle <mctmca@gmail.com>
2013-11-09 17:32:14 -0500

    lexer finished; expecting change to special_chars table from addition of query rules to spec.txt

---
+++ lexer.py
@@ -0,0 +1,57 @@
+special_chars = {".":"seperator-statement",
+		":":"seperator-name",
+		"?":"quantifier-universal",
+		"!":"quantifier-existential",
+		"/":"seperator-sound-change",
+		"_":"seperator-environment",
+		"[":"begin-feature-vector",
+		"]":"end-feature-vector",
+		"+":"feature-positive",
+		"-":"feature-negative",
+		"{":"begin-group",
+		"}":"end-group",
+		",":"seperator-list",
+		">":"seperator-sonority"}
+null_type = "null"
+name_type = "name"
+
+# class to hold lex tokens
+class Token:
+	typestr = null_type
+	text = ""
+
+	def __init__(self, typestr_in, text_in):
+		self.typestr = typestr_in
+		self.text = text_in
+
+# strips all (including interior) whitespace from a string
+def stripwhite(string):
+	l = string.split()
+	ret = ""
+
+	for i in l:
+		ret += i
+
+# lexes a source string; returns an array of statements, which are arrays of tokens
+def lex(source):
+	ret = []
+
+	statement_list = source.split(".")
+	for statement in statement_list:
+		tok_list = []
+		current_name = ""
+
+		for char in statement:
+			if char in special_chars:
+				if current_name != "":
+					tok_list += [Token(name_type, current_name)]
+					current_name = ""
+
+				tok_list += [Token(special_chars[char], char)]
+			else:
+				current_name += char
+
+		tok_list += [Token(name_type, current_name)]
+		ret += [tok_list]
+
+	return ret
